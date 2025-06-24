0634c7f0fccd3ba5fbf4a37c6826ee0b55d1cb6b
50101063 <gokuls1@ltfs.com>
2025-06-24 10:27:36 +0530

    Delete frontend directory

--- frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://your-api-gateway-url/development'
- // This is a placeholder URL for development. Replace with your actual API Gateway development url.
-};
