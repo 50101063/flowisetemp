4491a78c908e9e5c672099a49eb55f15ee053cf0
50101063 <gokuls1@ltfs.com>
2025-06-24 10:32:30 +0530

    FE: Add app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+G@omponent({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppPageComponent {
+  title = 'Frontend Application';
+}
