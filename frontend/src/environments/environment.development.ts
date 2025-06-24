70fa7e9e4baab93632f96d59e07a3e0273d8f444
50101063 <gokuls1@ltfs.com>
2025-06-24 10:23:34 +0530

    feat: Add environment.development.ts

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://your-api-gateway-url/development'
+ // This is a placeholder URL for development. Replace with your actual API Gateway development url.
+};
