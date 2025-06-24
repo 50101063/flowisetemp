c1a00265d0565a94671bd670ddb2d4f7f314e01f
50101063 <gokuls1@ltfs.com>
2025-06-24 10:54:03 +0530

    Create main Spring Boot application file.

---
+++ backend/src/main/java/com/onboardingsystem/digital/onboardingsystem/OnboardingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.onboardingsystem.digital.onboardingsystem;
+
+Import org.springframework.boot.SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OnboardingSystemApplcation
+
+ public class OnboardingSystemApplcation {
+
+    public static void main(String[] args) {
+        SpringBootApplication.run(OnboardingSystemApplication.class, args);
+    }
+}
