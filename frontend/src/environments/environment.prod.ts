d4ebf2c5d46e77bc885388d77f7725e834aadf2e
50101063 <gokuls1@ltfs.com>
2025-06-24 10:56:59 +0530

    Delete frontend directory

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: https://your-production-api.com/api, // Replace with your production API URL: Add other production-specific configurations here
-};
