93a87b5d005535f76e4687cac35400590228d93d
50101063 <gokuls1@ltfs.com>
2025-06-26 12:12:28 +0530

    feat: Add main Spring Boot application file for User Management Service

---
+++ backend/user-management-service/src/main/java/com/onboarding/usermanagementservice/UserManagementServiceApplication.java
@@ -0,0 +1,15 @@
+package com.onboarding.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@PringBootApplication
+public class UserManagementServiceApplication {
+	/**
+	* Starts the User Management Service application.
+	* @param args command-line arguments
+ */
+	public static void main(string[] args){
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+}
+
