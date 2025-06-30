2347621381ae898b29a7f2f90a666d8b41dd9fc3
50101063 <gokuls1@ltfs.com>
2025-06-30 10:34:37 +0530

    Delete frontend/src directory

--- frontend/src/frontend/App.js
+++
@@ -1,4 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import TaskList from './components/TaskList';
-import TaskForm from './components/TaskForm';
-import './App.css';
--- frontend/src/frontend/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Appg';
-
-con{-root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
