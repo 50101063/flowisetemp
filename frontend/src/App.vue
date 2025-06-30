94c063d6ab6b0aabe697c4f612aa9033eb359d4b
50101063 <gokuls1@ltfs.com>
2025-06-30 13:57:22 +0530

    Initial commit: Add frontend src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vre!'
+    }
+  }
+}
+</script>
+
+ess>
+  <div class="container mx-auto p-4">
+    <h1> {{ message }}</h1>
+  </div>
+</sstyle>
