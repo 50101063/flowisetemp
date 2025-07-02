da292961cde1a2435d9daa8542dacb660d9f71f9
50101063 <gokuls1@ltfs.com>
2025-07-02 12:38:32 +0530

    feat: Initial frontend project setup: tailwind.config.js (temporary commit to main due to branch creation limitation)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} **/
+module.exports = {
+  content: [
+    './src/app/**/*.js,tHjsx,tsx,mdx'',
+    './src/pages/**/*.js,tHjsx,tsx,mdx'',
+    './src/components/**/*.js,tsijsx,tsx,mdx'',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
