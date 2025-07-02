ffbe81523d7d5de0d24dcaf81442cf75413580c0
50101063 <gokuls1@ltfs.com>
2025-07-02 15:49:05 +0530

    feat: Add src/components/NavBar.vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="js">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <nav class="nav">
+    <router-link to="/">Task List</router-link>
+    <router-link to="/add-task">Add Task</router-linkh>
+  </nav>
+</templbate>
+
+<style scoped role="scss2">
+
+</style>
