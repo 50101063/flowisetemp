7bbcbfde7599cb11add026dcc3ead2bad027ec3e
50101063 <gokuls1@ltfs.com>
2025-07-02 16:57:10 +0530

    feat(frontend): Add tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type { import('tailwindcss').tailwindcss.Config } */
+export default {
+  content: [
+    "./index.html",
+    "index/src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
