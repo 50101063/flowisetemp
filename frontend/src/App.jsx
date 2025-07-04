2edd5882696ccffb0606f3369c5239b5e39eb7bf
50101063 <gokuls1@ltfs.com>
2025-07-04 17:54:52 +0530

    FE: Add App.jsx with React Router setup and basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,26 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
+import RecipeForm from './components/RecipeForm';
 import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import RecipeListPage from './pages/RecipeListPage';
-import RecipeDetailPage from './pages/RecipeDetailPage';
-import RecipeFormPage from './pages/RecipeFormPage';
-import NotFoundPage from './pages/NotFoundPage';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="min-h-screen bg-gray-100 flex flex-col">
-          <Navbar />
-          <main className="flex-grow container mx-auto p-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-
-              {/* Protected Routes */}
-              <Route element={<PrivateRoute />}>
-                <Route path="/recipes" element={<RecipeListPage />} />
-                <Route path="/recipes/:id" element={<RecipeDetailPage />} />
-                <Route path="/recipes/new" element={<RecipeFormPage />} />
-                <Route path="/recipes/edit/:id" element={<RecipeFormPage />} />
-              </Route>
-
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/recipes" element={<RecipeList />} />
+            <Route path="/recipes/:id" element={<RecipeDetail />} />
+            <Route path="/recipes/new" element={<RecipeForm />} />
+            <Route path="/recipes/edit/:id" element={<RecipeForm />} />
+            <Route path="/" element={<LoginForm />} /> {/* Default route */}
+          </Routes>
         </div>
       </AuthProvider>
     </Router>
