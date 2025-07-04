17934787f00153791f091ced7c0c5b819a68e401
50101063 <gokuls1@ltfs.com>
2025-07-04 17:52:45 +0530

    feat: Initial frontend setup. Adds src/App.jsx with basic routing and AuthProvider.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { RecipeProvider } from './context/RecipeContext';
-import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import AddEditRecipePage from './pages/AddEditRecipePage';
+import RecipeListPage from './pages/RecipeListPage';
 import RecipeDetailPage from './pages/RecipeDetailPage';
+import RecipeFormPage from './pages/RecipeFormPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <RecipeProvider>
-          <div className="min-h-screen bg-gray-100 flex flex-col">
-            <Navbar />
-            <main className="flex-grow container mx-auto p-4">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/recipes/new" element={<AddEditRecipePage />} />
+        <div className="min-h-screen bg-gray-100 flex flex-col">
+          <Navbar />
+          <main className="flex-grow container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+
+              {/* Protected Routes */}
+              <Route element={<PrivateRoute />}>
+                <Route path="/recipes" element={<RecipeListPage />} />
                 <Route path="/recipes/:id" element={<RecipeDetailPage />} />
-                <Route path="/recipes/:id/edit" element={<AddEditRecipePage />} />
-              </Routes>
-            </main>
-          </div>
-        </RecipeProvider>
+                <Route path="/recipes/new" element={<RecipeFormPage />} />
+                <Route path="/recipes/edit/:id" element={<RecipeFormPage />} />
+              </Route>
+
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+        </div>
       </AuthProvider>
     </Router>
   );
